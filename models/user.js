0fba81e2b265c497d876d2a347d2fd130f6cf28f
agret48 <167704067+agret48@users.noreply.github.com>
2024-04-26 18:49:18 +0200

    Update user.js

--- models/user.js
+++ models/user.js
@@ -14,7 +14,7 @@ module.exports = (sequelize, Sequelize) => {
         type: Sequelize.STRING
       },
       link: {
-        type: Sequelize.STRING
+         type: Sequelize.TEXT
       }
     });
   
